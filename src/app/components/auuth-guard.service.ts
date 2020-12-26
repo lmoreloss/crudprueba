a3eafb95c3d99559012f37716e3f9e42c5cf9238
Luis <lmoreloss@gmail.com>
2020-12-26 02:02:01 -0600

    cambiado al guard de angular por default

--- src/app/components/auuth-guard.service.ts
+++ src/app/components/auuth-guard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from "./auth.service";
-import { Router, CanActivate } from "@angular/router";
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +9,14 @@ export class AuuthGuardService implements CanActivate {
 
   constructor(private router: Router, private auth: AuthService) { }
 
-  canActivate() { 
-    if (this.auth.estaLoggeado()) {
-      return true;
-    } 
-    this.router.navigateByUrl('/login');
-    console.log("redir de guard");
-      return false;
-    
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean {
+      if (this.auth.estaLoggeado()) {
+        return true;
+      } else {
+        this.router.navigate(['login']);
+        return false;
+      }
   }
 }
